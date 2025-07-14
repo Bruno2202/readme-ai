19718930d5ac9f3386fde0014713a014a51c83d6
Bruno2202 <brunoterribile2005@gmail.com>
2025-07-14 17:13:22 -0300

    :sparkles: Add more file extensions to be read
    • Include additional extensions in the file reading logic

--- src/config/files.ts
+++ src/config/files.ts
@@ -1,5 +1,45 @@
 // Extensões válidas para leitura
-export const VALID_EXTENSIONS = ['ts', 'js', 'tsx', 'jsx', 'json', 'md', 'py'];
+export const VALID_EXTENSIONS = [
+    'ts',
+    'js',
+    'tsx',
+    'jsx',
+    'json',
+    'md',
+    'py',
+    'html',
+    'css',
+    'scss',
+    'yml',
+    'yaml',
+    'env',
+    'txt',
+    'sh',
+    'xml',
+    'toml',
+    'ini',
+    'rs',
+    'go',
+    'java',
+    'c',
+    'cpp',
+    'cs',
+    'php',
+    'rb',
+    'sql',
+];
+
+// Arquivos que serão lidos
+export const VALID_FILENAMES = [
+    'LICENSE',
+    'LICENSE.md',
+    'LICENSE.txt',
+    'LICENCE',
+    'COPYING',
+    'README',
+    'README.md',
+    'README.txt'
+];
 
 // Arquivos que serão ignorados
-export const IGNORED_PATHS = ["node_modules", ".git", "dist", "output", "readme-ai"]; 
+export const IGNORED_PATHS = ["node_modules", ".git", "dist"]; 
